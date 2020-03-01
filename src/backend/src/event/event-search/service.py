d6b26100d3fd2acdf9c4bf20d57b4458c2b140d0
NoahTolentinoNguyen <ntolentino-nguyen@csumb.edu>
2020-02-29 18:08:19 -0800

    Initial Event Search Commit

---
+++ src/backend/src/event/event-search/service.py
@@ -0,0 +1,34 @@
+import json
+import os
+import sys
+# makes 'lib' directory visible for external dependencies
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
+from googleapiclient.discovery import build
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']='credentials.json'
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+def lambda_handler(event, context):
+    service = build('calendar', 'v3', cache_discovery=False)
+    page_token = None
+    event_list = []
+
+    # get paginated list of event ids
+    while True:
+        event_page = service.events().list(calendarId=os.environ['EMAIL'], pageToken=page_token, q=event['queryStringParameters']['q']).execute()
+        for item in event_page['items']:
+            event_list.append(item['id'])
+        page_token = event_page.get('nextPageToken')
+        if not page_token:
+            break
+    
+    return respond(None, f"{event_list}")
+    
