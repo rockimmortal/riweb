c848d4c1a13c6d4bc67808df160a6b9f37eed6a2
uncleurdnot <qminora@Gmail.com>
2020-03-04 11:20:37 -0800

    Updated code, still not 100% until we have an actual DB to work with

--- RDS_Handler.py
+++ RDS_Handler.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template, request
+#Adapted from Noah's event-create
+import json
+import os
+import sys
+# makes 'lib' directory visible for external dependencies
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
+import mysql.connector
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-	return render_template('index.html')
+eventdb = mysql.connector.connect(
+  host="localhost", #Assuming this is an IP needed
+  user="yourusername", #See next comment
+  passwd="yourpassword", #Let's find a way to *not* have this be in the source code.
+  database="RIDB"
+)
 
-@app.route('/', methods=['POST'])
-def getval():
-	name = request.form['name']
-	return render_template('pass.html', n=name)
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
 
-if __name__ == '__main__':
-	app.run(debug=True)
+def lambda_handler(event, context):
+	event = json.loads(event['body'])
+    c = eventdb.cursor()
+	sql = "INSERT INTO customers (summary, location, description, start, end) VALUES (%s, %s, %s, %s, %s)" #Need to see if there is a %(date)
+	val = (event['summary'], event['location'], event['description'], event['startdate'], event['enddate'])
+	mycursor.execute(sql, val)
+
+	eventdb.commit()
+	return respond(None, f"Entry Added")
