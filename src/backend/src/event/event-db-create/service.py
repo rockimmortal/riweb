10b647c8a15d53ac9bf553e0e01cdea82f905eed
uncleurdnot <qminora@Gmail.com>
2020-03-25 15:27:23 -0700

    Redo on the commit. Not sure if get_secret() needs to return something, or if it's just gonna edit DB_PASSWORD when called, or if it even needs to be called, or if AWS just knows.  Looking into it

--- src/backend/src/event/event-db-create/service.py
+++ src/backend/src/event/event-db-create/service.py
@@ -4,11 +4,64 @@ import sys
 # makes 'lib' directory visible for external dependencies
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
 import pymysql
+import boto3
+import base64
+from botocore.exceptions import ClientError
 
-db_host=os.environ["DB_HOST"]
-user=os.environ["DB_USER"]
-password=os.environ["DB_PASSWORD"]
-db_name=os.environ["DB_NAME"]
+
+def get_secret():
+
+    secret_name = "RIDEVAD"
+    region_name = "us-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+
+db_host=os.environ['RDS_HOST']
+user=os.environ['RDS_USERNAME']
+password=os.environ['RDS_PASSWORD']
+db_name=os.environ['RDS_DB_NAME']
 
 def respond(err, res=None):
     return {
@@ -25,7 +78,7 @@ def lambda_handler(event, context):
     result = []
     conn = pymysql.connect(db_host, user=user, passwd=password, db=db_name, connect_timeout=5)
     with conn.cursor() as cur:
-        cur.execute("INSERT INTO events (summary, location, description, startDate, endDate)" 
+        cur.execute("INSERT INTO events (summary, location, description, startDate, endDate)" #Make sure this is ESCAPED in front-end.  Possibly do a check on the backend as well, though this may be redundant.  We'd be looking for " ;-- "
                     f"VALUES ({event['summary']}, {event['location']}, {event['description']}, {event['startDate']}, {event['endDate']})")
         conn.commit()
         cur.close()
