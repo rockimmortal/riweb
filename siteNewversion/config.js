a6a34858a0858562187c0defd257f365301d8e14
Gurjot Sandhu <gsandhu@csumb.edu>
2020-03-28 17:48:10 -0700

    Add files via upload
    Updating site pages

---
+++ siteNewversion/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: 'ROCK IMMORTAL', // Site title.
+  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: 'Rock Immortal.', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: 'Rock Immortal', // Disqus shortname.
+  userName: 'Rock Immortal',
+  userTwitter: 'rockimmortal',
+  userLocation: 'Rock, Immortal',
+  userDescription: '',
+  copyright: ' Copyright © Rock Immortal. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#000000', // Used for setting manifest background color.
+}
