975d0a2d1a1bca77cc997390829fc8517a6c029d
NoahTN <ntolentino-nguyen@csumb.edu>
2020-04-06 15:22:00 -0700

    Update to pull from DB

--- src/backend/src/event/event-search/service.py
+++ src/backend/src/event/event-search/service.py
@@ -5,8 +5,6 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
 import pymysql
 
-# Will end up using Secrets Manager instead
-REGION=os.environ["REGION"]
 db_host=os.environ["DB_HOST"]
 user=os.environ["DB_USER"]
 password=os.environ["DB_PASSWORD"]
@@ -21,19 +19,34 @@ def respond(err, res=None):
         },
     }
 
-def lambda_handler(event, context):
-    event = json.loads(event['body'])
+def get_params(event):
+    query = ''
+    entry_count = 10
     
+    if 'queryStringParameters' in event:
+        if 'q' in event['queryStringParameters']:
+            query = event['queryStringParameters']['q']
+        if 'entry_count' in event['queryStringParameters']:
+            entry_count = event['queryStringParameters']['entry_count']
+    return (query, entry_count)
+    
+def db_search(event, query, entry_count):
     result = []
     conn = pymysql.connect(db_host, user=user, passwd=password, db=db_name, connect_timeout=5)
     with conn.cursor() as cur:
-        cur.execute("INSERT INTO event (summary, location, description, startDate, endDate) "
-                    "VALUES ('%s', '%s', '%s', '%s', '%s')" % (event['summary'], event['location'], event['description'], event['startDate'], event['endDate']))
-        cur.execute("SELECT * FROM event") # Outputs all entries for testing purposes
-        conn.commit()
-        cur.close()
+        cur.execute(f"""
+                    SELECT * FROM event 
+                    WHERE summary LIKE '%{query}%' OR description LIKE '%{query}%'
+                    LIMIT {entry_count}
+                    """)
         for row in cur:
             result.append(list(row))
             
     conn.close()
+    return result
+
+def lambda_handler(event, context):
+    params = get_params(event)
+    result = db_search(event, params[0], params[1])
+    
     return respond(None, result)
